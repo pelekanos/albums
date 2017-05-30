79341b000ee41ac3ad9f01a77c4339d908dd1a2b
dimitris <dmtrspelekanos@gmail.com>
2017-05-30 21:25:10 +0300

    lots

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,55 @@
+function searchFunction() {
+    document.getElementById('search-1').style.cssText = 'display:none';
+    document.getElementById('search-2').style.cssText = 'display:inline-block';
+}
+
+function ratingStars(x) {
+    if (x === 1) {
+        document.getElementById('star-1').style.cssText = 'display:none';
+        document.getElementById('star-2').style.cssText = 'display:inline-block';
+    }
+
+    if (x === 2) {
+        document.getElementById('star-1').style.cssText = 'display:none';
+        document.getElementById('star-2').style.cssText = 'display:inline-block';
+        document.getElementById('star-3').style.cssText = 'display:none';
+        document.getElementById('star-4').style.cssText = 'display:inline-block';
+
+    }
+
+    if (x === 3) {
+        document.getElementById('star-1').style.cssText = 'display:none';
+        document.getElementById('star-2').style.cssText = 'display:inline-block';
+        document.getElementById('star-3').style.cssText = 'display:none';
+        document.getElementById('star-4').style.cssText = 'display:inline-block';
+        document.getElementById('star-5').style.cssText = 'display:none';
+        document.getElementById('star-6').style.cssText = 'display:inline-block';
+
+    }
+
+    if (x === 4) {
+        document.getElementById('star-1').style.cssText = 'display:none';
+        document.getElementById('star-2').style.cssText = 'display:inline-block';
+        document.getElementById('star-3').style.cssText = 'display:none';
+        document.getElementById('star-4').style.cssText = 'display:inline-block';
+        document.getElementById('star-5').style.cssText = 'display:none';
+        document.getElementById('star-6').style.cssText = 'display:inline-block';
+        document.getElementById('star-7').style.cssText = 'display:none';
+        document.getElementById('star-8').style.cssText = 'display:inline-block';
+
+    }
+
+    if (x === 5) {
+        document.getElementById('star-1').style.cssText = 'display:none';
+        document.getElementById('star-2').style.cssText = 'display:inline-block';
+        document.getElementById('star-3').style.cssText = 'display:none';
+        document.getElementById('star-4').style.cssText = 'display:inline-block';
+        document.getElementById('star-5').style.cssText = 'display:none';
+        document.getElementById('star-6').style.cssText = 'display:inline-block';
+        document.getElementById('star-7').style.cssText = 'display:none';
+        document.getElementById('star-8').style.cssText = 'display:inline-block';
+        document.getElementById('star-9').style.cssText = 'display:none';
+        document.getElementById('star-10').style.cssText = 'display:inline-block';
+
+    }
+}
